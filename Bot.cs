652a1c8d72664ea767a211ed916b3fe87cf81ca6
Jason <dinhjason0@gmail.com>
2021-05-29 02:38:59 -0700

    Coding Bootcamp? KekW

--- Bot.cs
+++ Bot.cs
@@ -14,6 +14,7 @@ using DSharpPlus.Interactivity;
 using DSharpPlus.Interactivity.Extensions;
 using DSharpPlus.Entities;
 using Yuzuri.Commons;
+using Yuzuri.bin;
 
 namespace Yuzuri
 {
@@ -40,7 +41,7 @@ namespace Yuzuri
             {
                 var discordConfig = new DiscordConfiguration
                 {
-                    Token = Config.Token,
+                    Token = staticToken.Token,
                     TokenType = TokenType.Bot,
                     AutoReconnect = true,
                     //MinimumLogLevel = LogLevel.Debug
