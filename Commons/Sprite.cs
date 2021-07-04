0422c4549e1790693903bb26b16d4386613cb182
Jason <dinhjason0@gmail.com>
2021-07-04 05:30:04 -0700

    Pushing updates to pull
    Chew me out

--- Commons/Sprite.cs
+++ Commons/Sprite.cs
@@ -1,4 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Png;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
 using Yuzuri.Managers;
 
 namespace Yuzuri.Commons
@@ -7,12 +16,19 @@ namespace Yuzuri.Commons
     {
         public string SpriteName { get; set; }
         public List<int> SpriteCoords { get; set; }
+        public Rectangle CroppedSize { get; set; }
 
         public Sprite(string name)
         {
             SpriteName = name;
-            ImageProcesserManager imageProcesser = new ImageProcesserManager();
-            SpriteCoords = imageProcesser.SpriteDestination(name);
+            SpriteCoords = SpriteDestination();
+            EditSpriteInfo();
+        }
+
+        public Sprite(List<int> spriteCoords)
+        {
+            SpriteCoords = spriteCoords;
+            EditSpriteInfo();
         }
 
         public string GetSpriteName()
@@ -33,6 +49,192 @@ namespace Yuzuri.Commons
         public int GetSpriteCoordY()
         {
             return SpriteCoords[1];
+        }
+
+
+        public void EditSpriteInfo()
+        {
+            using StreamReader r = new StreamReader($"data/Sprite_Resources/PlayerSheetAssistant.json");
+            string json = r.ReadToEnd();
+            string[] checkToSpace = json.Split("\n");
+            r.Close();
+            json = "";
+            JsonManager jsonManager = new JsonManager();
+            //SpriteName is checked if exists
+            //If it does, find the sprite's name, grab the coordinates, and do the sprite-sheet writing from there-on
+            //-SpriteName is checked to see if it is a player
+            //-If it is a player, see if there is an 'available__loading' name in the list
+            //--If there is, replace the 'available__loading' name in the list with the Sprite's name and write the Sprite's information into the sheet
+            //--If there isn't, write the sprite's information on the line immediately after, and then write the file back to the sheet
+
+            //If it isn't a player~~~~~
+            if (jsonManager.SpriteExistCheck(SpriteName) != true)
+            {
+                int key;
+                if (SpriteName.Length == 18 && int.TryParse(SpriteName, out key))
+                {
+                    if (jsonManager.SpriteExistCheck("available__loading"))
+                    for (int i = 0; i < checkToSpace.Length; i++)
+                    {
+                        if (checkToSpace[i] == "available__loading")
+                        {
+                            checkToSpace[i] = $"{checkToSpace[i].Replace("available__loading")}"
+                            foreach (string str in checkToSpace)
+                            {
+                                json += $"{str}\n";
+                            }
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        for (int i = 0; i < checkToSpace.Length; i++)
+                        {
+                            if (i == checkToSpace.Length - 2)
+                                json += $"{checkToSpace[i].Replace("\r", "")},\n\t\"{SpriteName}\": [ {SpriteCoords[0]} , {SpriteCoords[1]} ]\n";
+                            else if (i == checkToSpace.Length - 1)
+                                json += $"}}";
+                            else
+                                json += $"{checkToSpace[i]}\n";
+                        }
+                    }
+                }
+            }
+            else
+            {
+                foreach (string str in checkToSpace)
+                {
+                    if (str.Equals($"}}") == false)
+                        json += $"{str}\n";
+                    else
+                    {
+                        json += $"\t\"{SpriteName}\": [ {SpriteCoords[0]}, {SpriteCoords[1]} ]\n}}";
+                    }
+                }
+            }
+            Console.WriteLine($"\n{json}\n");
+            File.WriteAllText(@"data/Sprite_Resources/PlayerSheetAssistant.json", json);
+        }
+
+        //This exists specifically for empty PlayerSpriteSheets - catching the worst case scenario
+        //public void AddSpriteInfo()
+        //{
+        //    using StreamReader r = new StreamReader($"data/Sprite_Resources/PlayerSheetAssistant.json");
+        //    string json = r.ReadToEnd();
+        //    r.Close();
+        //    Console.WriteLine($"\n{json}\n");
+        //    File.WriteAllText(@"data/Sprite_Resources/PlayerSheetAssistant.json", json);
+        //}
+
+        public List<int> SpriteDestination()
+        {
+            using (StreamReader reader = new StreamReader($"data/Sprite_Resources/PlayerSheetAssistant.json"))
+            {
+                Console.WriteLine($"This is the target being searched: >>>>{SpriteName}<<<<");
+                string content = reader.ReadToEnd();
+                reader.Close();
+                //Console.WriteLine("The listed coordinates are as follows:\n" + content);
+                var fullContent = (JObject)JsonConvert.DeserializeObject(content);
+                //Console.WriteLine("Summoned new JObject from Player Sheet Assistant\n" + fullContent);
+                List<int> coordinates = new List<int>();
+                coordinates.Add((int)fullContent[SpriteName][0]);
+                coordinates.Add((int)fullContent[SpriteName][1]);
+                Console.WriteLine($"{SpriteName} @ Coordinates: [{coordinates[0]},{coordinates[1]}]\n\n");
+                return coordinates;
+            }
+        }
+
+        public List<int> SpriteDestination(string spriteName)
+        {
+            using (StreamReader reader = new StreamReader($"data/Sprite_Resources/PlayerSheetAssistant.json"))
+            {
+                Console.WriteLine($"This is the target being searched: >>>>{spriteName}<<<<");
+                string content = reader.ReadToEnd();
+                reader.Close();
+                //Console.WriteLine("The listed coordinates are as follows:\n" + content);
+                var fullContent = (JObject)JsonConvert.DeserializeObject(content);
+                //Console.WriteLine("Summoned new JObject from Player Sheet Assistant\n" + fullContent);
+                List<int> coordinates = new List<int>();
+                coordinates.Add((int)fullContent[spriteName][0]);
+                coordinates.Add((int)fullContent[spriteName][1]);
+                Console.WriteLine($"{spriteName} @ Coordinates: [{coordinates[0]},{coordinates[1]}]\n\n");
+                return coordinates;
+            }
+        }
+
+        public Rectangle CropLocation()
+        {
+            //coords[0] = x, coords[1] = y
+            int localX = 0;
+            int localy = 0;
+            if (SpriteCoords[0] > 0)
+                localX = SpriteCoords[0] * 35;
+            if (SpriteCoords[1] > 0)
+                localy = SpriteCoords[1] * 35;
+            var CropSpace = new Rectangle(localX, localy, 35, 35);
+            return CropSpace;
+        }
+
+        public Rectangle CropLocation(int publicX, int publicY)
+        {
+            int localX = 0;
+            int localy = 0;
+            if (publicX > 0)
+                localX = publicX * 35;
+            if (publicY > 0)
+                localy = publicY * 35;
+            var CropSpace = new Rectangle(localX, localy, 35, 35);
+            return CropSpace;
+        }
+
+        public FileStream CompoundedMessage()
+        {
+            if (File.Exists($"data/Sprite_Resources/{SpriteName}temp.png"))
+                File.Delete($"data/Sprite_Resources/{SpriteName}temp.png");
+
+            using var fs = new FileStream($"data/Sprite_Resources/PlayerSheet.png", FileMode.Open, FileAccess.Read);
+            using MemoryStream outStream = new MemoryStream();
+            using var image = Image.Load(fs);
+            using (var fstemp = new FileStream($"data/Sprite_Resources/{SpriteName}temp.png", FileMode.CreateNew, FileAccess.ReadWrite))
+            {
+                var pngEncoder = new PngEncoder();
+                var clone = image.Clone(img => img
+                .Crop(CropLocation()));
+                clone.Save(outStream, pngEncoder);
+                Console.WriteLine("Cropped Image");
+                outStream.Position = 0;
+                outStream.CopyTo(fstemp);
+                Console.WriteLine("Generated Image");
+                fstemp.Close();
+                var fstemp2 = new FileStream($"data/Sprite_Resources/{SpriteName}temp.png", FileMode.Open, FileAccess.Read);
+                Console.WriteLine($"Read {SpriteName}");
+                return fstemp2;
+            }
+        }
+
+        public void WriteToSrpiteSheet()
+        {
+            using var image = Image.Load<Rgba32>($"data/Sprite_Resources/PlayerSheet.png");
+            using var fs = new FileStream($"data/Sprite_Resources/PlayerSheet.png", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            using Image<Rgba32> clone = image.Clone<Rgba32>(i => i.Crop(CropLocation()));
+            {
+                var pngEncoder = new PngEncoder();
+                image.Mutate(o => o.DrawImage(clone, new Point(SpriteCoords[0] * 35, SpriteCoords[1] * 35), 1f));
+                image.Save(fs, pngEncoder);
+                fs.Close();
+            }
+        }
+
+        public void RemovePlayerSpriteInfo(Player player)
+        {
+            using StreamReader r = new StreamReader($"data/Sprite_Resources/PlayerSheetAssistant.json");
+            string json = r.ReadToEnd();
+            r.Close();
+            StringBuilder jsonBuild = new StringBuilder(r.ReadToEnd());
+            jsonBuild.Replace(player.UserId.ToString(), "available__loading");
+            json = jsonBuild.ToString();
+            Console.WriteLine($"\n{json}\n");
+            File.WriteAllText(@"data/Sprite_Resources/PlayerSheetAssistant.json", json);
         }
     }
 }
---
+++ Managers/JsonManager.cs
@@ -0,0 +1,133 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Yuzuri.Commons;
+
+namespace Yuzuri.Managers
+{
+    class JsonManager
+    {
+        //Dimensions for Sprite Sheet tiles
+
+        public List<List<int>> SpriteDestinationList()
+        {
+            using (StreamReader reader = new StreamReader($"data/Sprite_Resources/PlayerSheetAssistant.json"))
+            {
+                List<List<int>> value = new List<List<int>>();
+                string content = reader.ReadToEnd();
+                reader.Close();
+                Console.WriteLine("The listed coordinates are as follows:\n" + content + "\n\n");
+                var contentListing = content.Split('\n');
+                for (var i = 0; i < contentListing.Length; i++)
+                {
+                    var matches = Regex.Matches(contentListing[i], "\"([^}]+)\"");
+                    foreach (Match match in matches)
+                    {
+                        contentListing[i] = match.Groups[1].Value;
+                    }
+                    Console.WriteLine(contentListing[i]);
+                    if (contentListing[i] == "{\r" || contentListing[i] == "}" || contentListing[i] == "{" || contentListing[i] == "}\r" || contentListing[i] == "}\n")
+                    { }
+                    else
+                        value.Add(SpriteDestination(contentListing[i]));
+                }
+                return value;
+            }
+        }
+
+        public List<string> SpriteNames()
+        {
+            using (StreamReader reader = new StreamReader($"data/Sprite_Resources/PlayerSheetAssistant.json"))
+            using (JsonTextReader fileContent = new JsonTextReader(reader))
+            {
+                List<string> value = new List<string>();
+                string content = reader.ReadToEnd();
+                reader.Close();
+                Console.WriteLine("The listed coordinates are as follows:\n" + content + "\n\n");
+                var contentListing = content.Split('\n');
+                for (var i = 0; i < contentListing.Length; i++)
+                {
+
+                    var matches = Regex.Matches(contentListing[i], "\"([^}]+)\"");
+                    foreach (Match match in matches)
+                    {
+                        contentListing[i] = match.Groups[1].Value;
+                    }
+                    Console.WriteLine(contentListing[i]);
+                    if (contentListing[i] == "{\r" || contentListing[i] == "}")
+                    { }
+                    else
+                        value.Add(contentListing[i]);
+                }
+                return value;
+            }
+        }
+
+        public List<Sprite> spriteRetrieval()
+        {
+            List<Sprite> sprites = new List<Sprite>();
+            List<string> spriteNames = SpriteNames();
+            foreach (string str in spriteNames)
+            {
+                sprites.Add(new Sprite(str));
+            }
+            return sprites;
+        }
+
+        public bool SpriteExistCheck(string target)
+        {
+            List<string> listToCheck = SpriteNames();
+            foreach (string str in listToCheck)
+            {
+                if (target == str)
+                    return true;
+            }
+            return false;
+        }
+
+        public void organisePlayerSheetAssistant()
+        {
+            //Heads - Column 1
+            //Torsos - Column 2
+            //Tall Straight Legs - Column 3 
+            //Short Straight Legs - Column 4 // Shoes - Column 8
+            //Tall Curve Legs - Column 5 // Shoes - Column 9
+            //Short Curve Legs - Column 6 // Shoes - Column 10
+            //Misc.Shoes - Column 11 // Players w/ Snowflake - 12
+
+            List<Sprite> sprites = spriteRetrieval();
+            string toJson = "{\n";
+            for (int i = 0; i <= 12; i++)
+            {
+                List<Sprite> organiseChunk = new List<Sprite>();
+                foreach (Sprite spr in sprites)
+                {
+                    if (i == spr.GetSpriteCoordX())
+                        organiseChunk.Add(spr);
+                }
+                for (int g = 0; g < organiseChunk.Count - 1; g++)
+                {
+                    if (organiseChunk[g].GetSpriteCoordY() > organiseChunk[g + 1].GetSpriteCoordY())
+                    {
+                        Sprite rep = organiseChunk[g];
+                        organiseChunk[g] = organiseChunk[g + 1];
+                        organiseChunk[g + 1] = rep;
+                    }
+                }
+                foreach (Sprite spr in organiseChunk)
+                {
+                    toJson += $"\r\"{spr.GetSpriteName()}\": [ {spr.GetSpriteCoordX()}, {spr.GetSpriteCoordY()},\n]";
+                }
+            }
+            toJson += "}";
+            Console.WriteLine(toJson);
+            File.WriteAllText(@"data/Sprite_Resources/PlayerSheetAssistant.json", toJson);
+        }
+    }
+}
